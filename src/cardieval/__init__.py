50f4040d7d632023868d95b0145d925368f24b41
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 15:10:29 +0300

    Expose provenance API and bump version

--- src/cardieval/__init__.py
+++ src/cardieval/__init__.py
@@ -9,6 +9,7 @@ from .leaderboard import Leaderboard, LeaderboardEntry, build_leaderboard
 from .models import BenchmarkManifest, MetricResult, ModelComparison, PredictionRecord, SubgroupResult
 from .multiple_testing import benjamini_hochberg, bonferroni
 from .ranking import hit_rate_at_k, ndcg_at_k, reciprocal_rank
+from .provenance import artifact_manifest, canonical_json_hash, evaluation_fingerprint, sha256_text
 from .registry import BenchmarkTask, TaskRegistry
 from .robustness import relative_drop, subgroup_robustness
 from .stress import StressResult, aggregate_stress, compare_stress
@@ -16,6 +17,7 @@ from .stress import StressResult, aggregate_stress, compare_stress
 __all__ = [
     "BenchmarkManifest",
     "BenchmarkTask",
+    "CalibrationBin",
     "CalibrationBin",
     "EvaluationReport",
     "Leaderboard",
@@ -27,21 +29,25 @@ __all__ = [
     "SubgroupResult",
     "TaskRegistry",
     "aggregate_stress",
+    "artifact_manifest",
     "benjamini_hochberg",
     "brier_score",
+    "canonical_json_hash",
     "bonferroni",
     "build_leaderboard",
     "calibration_curve",
     "compare_predictions",
     "compare_stress",
     "evaluate_submission",
+    "evaluation_fingerprint",
     "expected_calibration_error",
     "hit_rate_at_k",
     "ndcg_at_k",
     "paired_difference_ci",
     "reciprocal_rank",
     "relative_drop",
+    "sha256_text",
     "subgroup_robustness",
 ]
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
