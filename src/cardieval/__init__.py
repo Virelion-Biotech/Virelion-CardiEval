ea416b38b70105dc32d4123a14ff0353545ccd9d
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 14:37:14 +0300

    Expose CardiEval 0.2 evaluation APIs

--- src/cardieval/__init__.py
+++ src/cardieval/__init__.py
@@ -1,14 +1,21 @@
 """CardiEval: independent evaluation for cardiac challenge models."""
 
+from .calibration import brier_score, expected_calibration_error
+from .comparison import compare_predictions
 from .evaluator import EvaluationReport, evaluate_submission
-from .models import BenchmarkManifest, MetricResult, PredictionRecord
+from .models import BenchmarkManifest, MetricResult, ModelComparison, PredictionRecord, SubgroupResult
 
 __all__ = [
     "BenchmarkManifest",
     "EvaluationReport",
     "MetricResult",
+    "ModelComparison",
     "PredictionRecord",
+    "SubgroupResult",
+    "brier_score",
+    "compare_predictions",
     "evaluate_submission",
+    "expected_calibration_error",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
