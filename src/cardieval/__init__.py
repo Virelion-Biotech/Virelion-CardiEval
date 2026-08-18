e70ecfddf39e9950037577b28a4e8c697808c126
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 15:56:46 +0300

    Expose end-to-end pipeline APIs

--- src/cardieval/__init__.py
+++ src/cardieval/__init__.py
@@ -14,30 +14,34 @@ from .integrity import ArtifactRecord, ReleaseManifest, build_release_manifest, 
 from .leaderboard import Leaderboard, LeaderboardEntry, build_leaderboard
 from .models import BenchmarkManifest, MetricResult, ModelComparison, PredictionRecord, SubgroupResult
 from .multiple_testing import benjamini_hochberg, bonferroni
+from .pipeline import run_evaluation
 from .publication import LeaderboardSnapshot, ingest_bundles, load_bundle, publish_leaderboard, save_snapshot
 from .publication_history import LeaderboardDelta, PublicationComparison, compare_snapshots, load_snapshot, save_comparison, snapshot_hash
 from .ranking import hit_rate_at_k, ndcg_at_k, reciprocal_rank
 from .provenance import artifact_manifest, canonical_json_hash, evaluation_fingerprint, sha256_text
 from .registry import BenchmarkTask, TaskRegistry
 from .robustness import relative_drop, subgroup_robustness
+from .run_manifest import EvaluationRunManifest, build_run_manifest, save_run_manifest
 from .scorecard import BenchmarkScore, ModelScorecard, Scorecard, build_scorecard
 from .stress import StressResult, aggregate_stress, compare_stress
 
 __all__ = [
     "ArtifactRecord", "BenchmarkArtifact", "BenchmarkManifest", "BenchmarkPackage", "BenchmarkScore",
     "BenchmarkTask", "CalibrationBin", "ComparisonDecision", "ComparisonReport", "EvaluationReport",
-    "Leaderboard", "LeaderboardDelta", "LeaderboardEntry", "LeaderboardSnapshot", "MetricResult",
-    "ModelComparison", "ModelScorecard", "PredictionRecord", "PublicationComparison", "QualityGate",
-    "ReleaseGateReport", "ReleaseManifest", "Scorecard", "StressResult", "SubgroupResult", "SubmissionBundle",
-    "TaskRegistry", "aggregate_stress", "artifact_manifest", "benjamini_hochberg", "brier_score", "bonferroni",
-    "build_bundle", "build_comparison_report", "build_leaderboard", "build_release_manifest", "build_scorecard",
-    "calibration_curve", "canonical_json_hash", "cohen_kappa", "compare_predictions", "compare_snapshots",
-    "compare_stress", "confusion_matrix_counts", "decide_comparison", "evaluate_release_gates", "evaluate_submission",
-    "evaluation_fingerprint", "expected_calibration_error", "fingerprint_directory", "fingerprint_file", "hit_rate_at_k",
-    "ingest_bundles", "load_bundle", "load_package", "load_snapshot", "matthews_correlation", "negative_predictive_value",
-    "ndcg_at_k", "paired_difference_ci", "positive_predictive_value", "publish_leaderboard", "reciprocal_rank",
-    "relative_drop", "save_bundle", "save_comparison", "save_snapshot", "sensitivity", "sha256_text", "snapshot_hash",
-    "specificity", "subgroup_robustness", "validate_submission_against_package", "verify_package_artifacts", "verify_release_manifest",
+    "EvaluationRunManifest", "Leaderboard", "LeaderboardDelta", "LeaderboardEntry", "LeaderboardSnapshot",
+    "MetricResult", "ModelComparison", "ModelScorecard", "PredictionRecord", "PublicationComparison",
+    "QualityGate", "ReleaseGateReport", "ReleaseManifest", "Scorecard", "StressResult", "SubgroupResult",
+    "SubmissionBundle", "TaskRegistry", "aggregate_stress", "artifact_manifest", "benjamini_hochberg",
+    "brier_score", "bonferroni", "build_bundle", "build_comparison_report", "build_leaderboard",
+    "build_release_manifest", "build_run_manifest", "build_scorecard", "calibration_curve", "canonical_json_hash",
+    "cohen_kappa", "compare_predictions", "compare_snapshots", "compare_stress", "confusion_matrix_counts",
+    "decide_comparison", "evaluate_release_gates", "evaluate_submission", "evaluation_fingerprint",
+    "fingerprint_directory", "fingerprint_file", "hit_rate_at_k", "ingest_bundles", "load_bundle", "load_package",
+    "load_snapshot", "matthews_correlation", "negative_predictive_value", "ndcg_at_k", "paired_difference_ci",
+    "positive_predictive_value", "publish_leaderboard", "reciprocal_rank", "relative_drop", "run_evaluation",
+    "save_bundle", "save_comparison", "save_run_manifest", "save_snapshot", "sensitivity", "sha256_text",
+    "snapshot_hash", "specificity", "subgroup_robustness", "validate_submission_against_package",
+    "verify_package_artifacts", "verify_release_manifest",
 ]
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
