c071391a856e288b1bf65bb4b24f003f0ce86e0d
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 14:50:07 +0300

    Export ranking and robustness APIs

--- src/cardieval/__init__.py
+++ src/cardieval/__init__.py
@@ -6,7 +6,9 @@ from .evaluator import EvaluationReport, evaluate_submission
 from .leaderboard import Leaderboard, LeaderboardEntry, build_leaderboard
 from .models import BenchmarkManifest, MetricResult, ModelComparison, PredictionRecord, SubgroupResult
 from .multiple_testing import benjamini_hochberg, bonferroni
+from .ranking import hit_rate_at_k, ndcg_at_k, reciprocal_rank
 from .registry import BenchmarkTask, TaskRegistry
+from .robustness import relative_drop, subgroup_robustness
 
 __all__ = [
     "BenchmarkManifest",
@@ -26,6 +28,11 @@ __all__ = [
     "compare_predictions",
     "evaluate_submission",
     "expected_calibration_error",
+    "hit_rate_at_k",
+    "ndcg_at_k",
+    "reciprocal_rank",
+    "relative_drop",
+    "subgroup_robustness",
 ]
 
 __version__ = "0.3.0"
