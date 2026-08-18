fa69f06b3a287f8509cfebf024115c199f58bbe1
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 14:44:49 +0300

    Export leaderboard and multiple-testing APIs

--- src/cardieval/__init__.py
+++ src/cardieval/__init__.py
@@ -3,19 +3,26 @@
 from .calibration import brier_score, expected_calibration_error
 from .comparison import compare_predictions
 from .evaluator import EvaluationReport, evaluate_submission
+from .leaderboard import Leaderboard, LeaderboardEntry, build_leaderboard
 from .models import BenchmarkManifest, MetricResult, ModelComparison, PredictionRecord, SubgroupResult
+from .multiple_testing import benjamini_hochberg, bonferroni
 
 __all__ = [
     "BenchmarkManifest",
     "EvaluationReport",
+    "Leaderboard",
+    "LeaderboardEntry",
     "MetricResult",
     "ModelComparison",
     "PredictionRecord",
     "SubgroupResult",
+    "benjamini_hochberg",
     "brier_score",
+    "bonferroni",
+    "build_leaderboard",
     "compare_predictions",
     "evaluate_submission",
     "expected_calibration_error",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
